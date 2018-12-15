b6d723ff4f558b058b16e2c5b73a119782cb0125
gaufrere <aufrere.g@gmail.com>
2018-12-16 00:34:21 +0100

    feat(banner): custom banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,13 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Gilets Jaunes en Pays basque</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>Tu pourras trouver ici des informations nos moyens de communication,<br />
+                des documents qui nous semblent pertinents, et comment nous rejoindre.</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Commencer à naviquer</a></li>
                 </ul>
             </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Gilets jaunes - Pays basque"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
